f4ef19e2ca18da373035ef3fbaf529607e237e60
fbdthree <112531356+fbdthree@users.noreply.github.com>
2023-02-04 11:40:52 -0500

    models

---
+++ models/Users.js
@@ -0,0 +1,36 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: /.+@.+\.com/
+    },
+    thoughts: {
+      type: Schema.Types.ObjectId,
+      ref:"thoughts"
+    },
+    friends: {
+      type: Schema.Types.ObjectId,
+      ref:"users"
+    },
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+const Users = model('users', userSchema);
+
+module.exports = Users;
